a6de909e38085f01975b3a6e0e6202937e049740
TrNgTina <truongnguyentina@gmail.com>
2024-07-12 11:05:22 +0200

    Testing streamlit

--- main.py
+++ main.py
@@ -1,38 +1,18 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+from scipy.stats import norm
+import plotly.graph_objects as go
+from numpy import log, sqrt, exp  # Make sure to import these
+import matplotlib  # Importing only matplotlib
+import seaborn as sns
 
-# Set the title of the app
-st.title('Simple Data Visualization')
+x = st.slider('x')  # 👈 this is a widget
+st.write(x, 'squared is', x * x)
 
-# Description of the app
-st.write("This is a simple Streamlit app to visualize data using different chart types.")
+if st.checkbox('Show dataframe'):
+    chart_data = pd.DataFrame(
+       np.random.randn(20, 3),
+       columns=['a', 'b', 'c'])
 
-# Generate a random dataset
-data = pd.DataFrame(
-    np.random.randn(100, 3),
-    columns=['A', 'B', 'C']
-)
-
-# Display the dataframe
-st.write("Here is the dataset we are working with:")
-st.write(data)
-
-# Line chart
-st.write("Line chart of the dataset:")
-st.line_chart(data)
-
-# Area chart
-st.write("Area chart of the dataset:")
-st.area_chart(data)
-
-# Bar chart
-st.write("Bar chart of the dataset:")
-st.bar_chart(data)
-
-# Matplotlib chart
-st.write("Histogram of the dataset columns:")
-fig, ax = plt.subplots()
-data.hist(ax=ax)
-st.pyplot(fig)
+    chart_data
